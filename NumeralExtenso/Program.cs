40caf780d7707526134ef5e868e87767c51a80b7
MWLeonel <wmleonel6@gmail.com>
2020-09-25 00:53:54 -0300

    Numeros em Palavras

---
+++ NumeralExtenso/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string palavra;
+
+            Console.Write("Digite um Número: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            palavra = numero.ToWords();
+            Console.WriteLine("Seu numero em palavras");
+            Console.WriteLine(palavra);
+        }
+    }
+}
